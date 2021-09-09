84478f118cfa1e19da6fe522640fb1d4b811b297
prabhat <akoup66@gmail.com>
2021-09-09 13:31:18 +0545

    additional improvements

--- src/message.py
+++ src/message.py
@@ -4,6 +4,7 @@ message_dict = {
     "amount": "Enter amount for",
     "shortage_error": "We don't have enough stock. Please go below",
     "response": "Do you want to order again?(y/n)",
-    "success_order_details": "Your order has been placed successfully \nYour order items is mentioned below"
+    "success_order_details": "Your order has been placed successfully \nYour order items is mentioned below",
+    "add_stock": "We are running low on {}. Do you want to add flowers in stock?(y/n)"
 }
 
--- src/stock_manage.py
+++ src/stock_manage.py
@@ -1,5 +1,5 @@
 from src.constants import STOCK_DICT, ADD_STOCK_AMOUNT
-
+from src.message import message_dict
 
 # adds the stock if the flower is below the threshold amount
 def add_stock(flower):
@@ -16,4 +16,5 @@ def update_stock(amount_to_reduce_dict):
 def check_stock():
     for flower, quantity in STOCK_DICT.items():
         if STOCK_DICT[flower] <= 30:
-            add_stock(flower)
+            if input("{}".format(message_dict['add_stock'], flower)).upper() == "y":
+                add_stock(flower)
